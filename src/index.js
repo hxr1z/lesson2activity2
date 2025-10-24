9155660ceebfcdb3010b2bc0600cb6a86d189b80
hxr1z <06_postfix.feeds@icloud.com>
2025-10-24 20:13:51 +0800

    added prices

--- src/index.js
+++ src/index.js
@@ -10,26 +10,31 @@ function App() {
         name="Pizza Spinaci"
         ingredients="Tomato, mozzarella, spinach, and ricotta cheese"
         photo="/pizzas/spinaci.jpg"
+        price="$10.00"
       />
       <Pizza 
         name="Pizza Margherita"
         ingredients="Tomato, mozzarella, and basil"
         photo="/pizzas/margherita.jpg"
+        price="$8.00"
       />
       <Pizza 
         name="Pizza Funghi"
         ingredients="Tomato, mozzarella, and mushrooms"
         photo="/pizzas/funghi.jpg"
+        price="$9.00"
       />
       <Pizza 
         name="Pizza Prosciutto"
         ingredients="Tomato, mozzarella, ham, and arugula"
         photo="/pizzas/prosciutto.jpg"
+        price="$11.00"
       />
       <Pizza 
         name="Pizza Salamino"
         ingredients="Tomato, mozzarella, and pepperoni"
         photo="/pizzas/salamino.jpg"
+        price="$10.50"
       />
     </>
   );
@@ -45,7 +50,7 @@ function Pizza(props) {
       <img src={props.photo} alt={props.name} />
       <h2>{props.name}</h2>
       <p>{props.ingredients}</p>
-      
+      <p>{props.price}</p>
     </div>
   );
 }
