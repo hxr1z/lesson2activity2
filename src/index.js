6f561f8b3339e78ac611dc5a2b8d1a88cf671aba
hxr1z <06_postfix.feeds@icloud.com>
2025-10-24 16:03:10 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// App is the parent component
+function App() {
+  return (
+    <>
+      <Header />
+      <Pizza 
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozzarella, spinach, and ricotta cheese"
+        photo="/pizzas/spinaci.jpg"
+      />
+      <Pizza 
+        name="Pizza Margherita"
+        ingredients="Tomato, mozzarella, and basil"
+        photo="/pizzas/margherita.jpg"
+      />
+      <Pizza 
+        name="Pizza Funghi"
+        ingredients="Tomato, mozzarella, and mushrooms"
+        photo="/pizzas/funghi.jpg"
+      />
+      <Pizza 
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozzarella, ham, and arugula"
+        photo="/pizzas/prosciutto.jpg"
+      />
+      <Pizza 
+        name="Pizza Salamino"
+        ingredients="Tomato, mozzarella, and pepperoni"
+        photo="/pizzas/salamino.jpg"
+      />
+    </>
+  );
+}
+
+function Header() {
+  return <h1>Pizza@Hariz's</h1>;
+}
+
+function Pizza(props) {
+  return (
+    <div>
+      <img src={props.photo} alt={props.name} />
+      <h2>{props.name}</h2>
+      <p>{props.ingredients}</p>
+      
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
